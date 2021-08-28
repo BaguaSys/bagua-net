2c92a356d396b4374434d5a796351995cec21887
shjwudp <shjwudp@gmail.com>
2021-08-28 10:32:27 +0800

    ..

--- cc/v3/nccl_net_v3.h
+++ cc/v3/nccl_net_v3.h
@@ -4,59 +4,11 @@
  * See LICENSE.txt for license information
  ************************************************************************/
 
-#ifndef NCCL_NET_H_
-#define NCCL_NET_H_
+#pragma once
 
-/** COPY FROM nccl.h.in - START **/
-
-/* Error type */
-typedef enum { ncclSuccess                 =  0,
-               ncclUnhandledCudaError      =  1,
-               ncclSystemError             =  2,
-               ncclInternalError           =  3,
-               ncclInvalidArgument         =  4,
-               ncclInvalidUsage            =  5,
-               ncclNumResults              =  6 } ncclResult_t;
-
-/* Reduction operation selector */
-typedef enum { ncclSum        = 0,
-               ncclProd       = 1,
-               ncclMax        = 2,
-               ncclMin        = 3,
-               ncclAvg        = 4,
-               ncclNumOps     = 5 } ncclRedOp_t;
-
-/* Data types */
-typedef enum { ncclInt8       = 0, ncclChar       = 0,
-               ncclUint8      = 1,
-               ncclInt32      = 2, ncclInt        = 2,
-               ncclUint32     = 3,
-               ncclInt64      = 4,
-               ncclUint64     = 5,
-               ncclFloat16    = 6, ncclHalf       = 6,
-               ncclFloat32    = 7, ncclFloat      = 7,
-               ncclFloat64    = 8, ncclDouble     = 8,
-#if defined(__CUDA_BF16_TYPES_EXIST__)
-               ncclBfloat16   = 9,
-               ncclNumTypes   = 10
-#else
-               ncclNumTypes   = 9
-#endif
-} ncclDataType_t;
-
-/** COPY FROM nccl.h.in - END **/
+#include "nccl_types.h"
 
 #include <stdint.h>
-
-#define NCCL_NET_HANDLE_MAXSIZE 64
-
-#define NCCL_PTR_HOST 0x1
-#define NCCL_PTR_CUDA 0x2
-
-typedef enum {NCCL_LOG_NONE=0, NCCL_LOG_VERSION=1, NCCL_LOG_WARN=2, NCCL_LOG_INFO=3, NCCL_LOG_ABORT=4, NCCL_LOG_TRACE=5} ncclDebugLogLevel;
-typedef enum {NCCL_INIT=1, NCCL_COLL=2, NCCL_P2P=4, NCCL_SHM=8, NCCL_NET=16, NCCL_GRAPH=32, NCCL_TUNING=64, NCCL_ENV=128, NCCL_ALL=~0} ncclDebugLogSubSys;
-
-typedef void (*ncclDebugLogger_t)(ncclDebugLogLevel level, unsigned long flags, const char *file, int line, const char *fmt, ...);
 
 typedef struct {
   char* name;     // Used mostly for logging.
@@ -68,8 +20,6 @@ typedef struct {
   int port;       // Port number.
   int maxComms;   // Maximum number of comms we can create
 }ncclNetProperties_v3_t;
-
-typedef ncclNetProperties_v3_t ncclNetProperties_t;
 
 typedef struct {
   // Name of the network (mainly for logs)
@@ -110,10 +60,6 @@ typedef struct {
   ncclResult_t (*closeListen)(void* listenComm);
 } ncclNet_v3_t;
 
-typedef ncclNet_v3_t ncclNet_t;
-
-#define NCCL_PLUGIN_SYMBOL ncclNetPlugin_v3
-
 typedef struct {
   // Name of the collective network (mainly for logs)
   const char* name;
@@ -151,9 +97,3 @@ typedef struct {
   ncclResult_t (*closeColl)(void* collComm);
   ncclResult_t (*closeListen)(void* listenComm);
 } ncclCollNet_v3_t;
-
-typedef ncclCollNet_v3_t ncclCollNet_t;
-
-#define NCCL_COLLNET_PLUGIN_SYMBOL ncclCollNetPlugin_v3
-
-#endif // end include guard
