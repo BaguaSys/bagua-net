68ff5e46fe8e626be8103e5c5b3a3e20a378681a
shjwudp <shjwudp@gmail.com>
2021-08-28 10:35:13 +0800

    ..

--- cc/v3/nccl_net_v3.cc
+++ cc/v3/nccl_net_v3.cc
@@ -26,85 +26,85 @@ void set_properties(ncclNetProperties_v3_t &lhs, NCCLNetPropertiesC &rhs)
     lhs.maxComms = rhs.max_comms;
 }
 
-__hidden ncclResult_t baguaNetInit(ncclDebugLogger_t logFunction)
+__hidden ncclResult_t baguaNetInit_v3(ncclDebugLogger_t logFunction)
 {
     NCCL_DEBUG_LOG = logFunction;
     BaguaNet::instance();
 
-    NCCL_TRACE(NCCL_ALL, "baguaNetInit!");
+    NCCL_TRACE(NCCL_ALL, "baguaNetInit_v3!");
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetDevices(int *ndev)
+__hidden ncclResult_t baguaNetDevices_v3(int *ndev)
 {
     if (BaguaNet::instance().devices((int32_t *)ndev) != 0)
     {
-        NCCL_WARN("baguaNetDevices failed, ndev=%d", *ndev);
+        NCCL_WARN("baguaNetDevices_v3 failed, ndev=%d", *ndev);
         return ncclInternalError;
     }
 
-    NCCL_TRACE(NCCL_ALL, "baguaNetDevices, ndev=%d", *ndev);
+    NCCL_TRACE(NCCL_ALL, "baguaNetDevices_v3, ndev=%d", *ndev);
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetGetProperties(int dev, ncclNetProperties_v3_t *props)
+__hidden ncclResult_t baguaNetGetProperties_v3(int dev, ncclNetProperties_v3_t *props)
 {
     NCCLNetPropertiesC inner_props;
     int ret = BaguaNet::instance().get_properties(dev, &inner_props);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetGetProperties failed, ret=%d, dev=%d", ret, dev);
+        NCCL_WARN("baguaNetGetProperties_v3 failed, ret=%d, dev=%d", ret, dev);
         return ncclInternalError;
     }
     set_properties(*props, inner_props);
-    NCCL_TRACE(NCCL_ALL, "baguaNetGetProperties, dev=%d", dev);
+    NCCL_TRACE(NCCL_ALL, "baguaNetGetProperties_v3, dev=%d", dev);
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetListen(int dev, void *handle, void **listenComm)
+__hidden ncclResult_t baguaNetListen_v3(int dev, void *handle, void **listenComm)
 {
     int ret = BaguaNet::instance().listen(dev, handle, listenComm);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetListen failed, ret=%d, dev=%d", ret, dev);
+        NCCL_WARN("baguaNetListen_v3 failed, ret=%d, dev=%d", ret, dev);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetListen, dev=%d", dev);
+    NCCL_TRACE(NCCL_ALL, "baguaNetListen_v3, dev=%d", dev);
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetConnect(int dev, void *handle, void **sendComm)
+__hidden ncclResult_t baguaNetConnect_v3(int dev, void *handle, void **sendComm)
 {
     int ret = BaguaNet::instance().connect(dev, handle, sendComm);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetConnect failed, ret=%d", ret);
+        NCCL_WARN("baguaNetConnect_v3 failed, ret=%d", ret);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetConnect ok, dev=%d", dev);
+    NCCL_TRACE(NCCL_ALL, "baguaNetConnect_v3 ok, dev=%d", dev);
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetAccept(void *listenComm, void **recvComm)
+__hidden ncclResult_t baguaNetAccept_v3(void *listenComm, void **recvComm)
 {
     int ret = BaguaNet::instance().accept(listenComm, recvComm);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetAccept failed, ret=%d", ret);
+        NCCL_WARN("baguaNetAccept_v3 failed, ret=%d", ret);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetAccept, listenComm=%p", listenComm);
+    NCCL_TRACE(NCCL_ALL, "baguaNetAccept_v3, listenComm=%p", listenComm);
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetRegMr(void *comm, void *data, int size, int type, void **mhandle)
+__hidden ncclResult_t baguaNetRegMr_v3(void *comm, void *data, int size, int type, void **mhandle)
 {
-    NCCL_TRACE(NCCL_ALL, "baguaNetRegMr, comm=%p, data=%p, type=%d", comm, data, type);
+    NCCL_TRACE(NCCL_ALL, "baguaNetRegMr_v3, comm=%p, data=%p, type=%d", comm, data, type);
     return (type != NCCL_PTR_HOST) ? ncclInternalError : ncclSuccess;
 }
 
@@ -114,48 +114,48 @@ __hidden ncclResult_t baguaNetDeregMr(void *comm, void *mhandle)
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetIsend(void *sendComm, void *data, int size, void *mhandle, void **request)
+__hidden ncclResult_t baguaNetIsend_v3(void *sendComm, void *data, int size, void *mhandle, void **request)
 {
     int ret = BaguaNet::instance().isend(sendComm, data, size, mhandle, request);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetIsend failed, ret=%d, sendComm=%p, data=%p, size=%d", ret, sendComm, data, size);
+        NCCL_WARN("baguaNetIsend_v3 failed, ret=%d, sendComm=%p, data=%p, size=%d", ret, sendComm, data, size);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetIsend, sendComm=%p, data=%p, size=%d, request_id=%d",
+    NCCL_TRACE(NCCL_ALL, "baguaNetIsend_v3, sendComm=%p, data=%p, size=%d, request_id=%d",
               sendComm, data, size, *(uintptr_t *)(*request));
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetIrecv(void *recvComm, void *data, int size, void *mhandle, void **request)
+__hidden ncclResult_t baguaNetIrecv_v3(void *recvComm, void *data, int size, void *mhandle, void **request)
 {
     int ret = BaguaNet::instance().irecv(recvComm, data, size, mhandle, request);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetIrecv failed, ret=%d, sendComm=%p, data=%p, size=%d", ret, recvComm, data, size);
+        NCCL_WARN("baguaNetIrecv_v3 failed, ret=%d, sendComm=%p, data=%p, size=%d", ret, recvComm, data, size);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetIrecv, recvComm=%p, data=%p, size=%d, request_id=%d",
+    NCCL_TRACE(NCCL_ALL, "baguaNetIrecv_v3, recvComm=%p, data=%p, size=%d, request_id=%d",
               recvComm, data, size, *(uintptr_t *)(*request));
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetFlush(void *recvComm, void *data, int size, void *mhandle)
+__hidden ncclResult_t baguaNetFlush_v3(void *recvComm, void *data, int size, void *mhandle)
 {
     // We don't support CUDA pointers, so we don't need a flush operation
     return ncclInternalError;
 }
 
-__hidden ncclResult_t baguaNetTest(void *request, int *done, int *size)
+__hidden ncclResult_t baguaNetTest_v3(void *request, int *done, int *size)
 {
     bool b_done = false;
     uintptr_t nbytes = 0;
     int ret = BaguaNet::instance().test(request, &b_done, &nbytes);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetTest failed, ret=%d, request_id=%d",
+        NCCL_WARN("baguaNetTest_v3 failed, ret=%d, request_id=%d",
                   ret, *static_cast<uintptr_t *>(request));
         return ncclInternalError;
     }
@@ -165,62 +165,62 @@ __hidden ncclResult_t baguaNetTest(void *request, int *done, int *size)
         *size = nbytes;
         NCCL_TRACE(NCCL_ALL, "size=%d", *size);
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetTest, request_id=%d, done=%d, nbytes=%d",
+    NCCL_TRACE(NCCL_ALL, "baguaNetTest_v3, request_id=%d, done=%d, nbytes=%d",
               *static_cast<uintptr_t *>(request), *done, nbytes);
 
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetCloseSend(void *sendComm)
+__hidden ncclResult_t baguaNetCloseSend_v3(void *sendComm)
 {
     int ret = BaguaNet::instance().close_send(sendComm);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetCloseSend failed, ret=%d, sendComm=%p", ret, sendComm);
+        NCCL_WARN("baguaNetCloseSend_v3 failed, ret=%d, sendComm=%p", ret, sendComm);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetCloseSend, sendComm=%p", sendComm);
+    NCCL_TRACE(NCCL_ALL, "baguaNetCloseSend_v3, sendComm=%p", sendComm);
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetCloseRecv(void *recvComm)
+__hidden ncclResult_t baguaNetCloseRecv_v3(void *recvComm)
 {
     int ret = BaguaNet::instance().close_recv(recvComm);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetCloseRecv failed, ret=%d, recvComm=%p", ret, recvComm);
+        NCCL_WARN("baguaNetCloseRecv_v3 failed, ret=%d, recvComm=%p", ret, recvComm);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetCloseRecv, recvComm=%p", recvComm);
+    NCCL_TRACE(NCCL_ALL, "baguaNetCloseRecv_v3, recvComm=%p", recvComm);
     return ncclSuccess;
 }
 
-__hidden ncclResult_t baguaNetCloseListen(void *listenComm)
+__hidden ncclResult_t baguaNetCloseListen_v3(void *listenComm)
 {
     int ret = BaguaNet::instance().close_listen(listenComm);
     if (ret != 0)
     {
-        NCCL_WARN("baguaNetCloseListen failed, ret=%d, listenComm=%p", ret, listenComm);
+        NCCL_WARN("baguaNetCloseListen_v3 failed, ret=%d, listenComm=%p", ret, listenComm);
         return ncclInternalError;
     }
-    NCCL_TRACE(NCCL_ALL, "baguaNetCloseListen, listenComm=%p", listenComm);
+    NCCL_TRACE(NCCL_ALL, "baguaNetCloseListen_v3, listenComm=%p", listenComm);
     return ncclSuccess;
 }
 
 ncclNet_v3_t ncclNetPlugin_v3 = {
     "BaguaNet",
-    baguaNetInit,
-    baguaNetDevices,
-    baguaNetGetProperties,
-    baguaNetListen,
-    baguaNetConnect,
-    baguaNetAccept,
-    baguaNetRegMr,
+    baguaNetInit_v3,
+    baguaNetDevices_v3,
+    baguaNetGetProperties_v3,
+    baguaNetListen_v3,
+    baguaNetConnect_v3,
+    baguaNetAccept_v3,
+    baguaNetRegMr_v3,
     baguaNetDeregMr,
-    baguaNetIsend,
-    baguaNetIrecv,
-    baguaNetFlush,
-    baguaNetTest,
-    baguaNetCloseSend,
-    baguaNetCloseRecv,
-    baguaNetCloseListen};
+    baguaNetIsend_v3,
+    baguaNetIrecv_v3,
+    baguaNetFlush_v3,
+    baguaNetTest_v3,
+    baguaNetCloseSend_v3,
+    baguaNetCloseRecv_v3,
+    baguaNetCloseListen_v3};
