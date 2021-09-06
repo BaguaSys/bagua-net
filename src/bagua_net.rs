61858dd0ca2765f652429a3f170f1c099e6177bf
shjwudp <shjwudp@gmail.com>
2021-09-06 16:03:43 +0800

    ..

--- src/bagua_net.rs
+++ src/bagua_net.rs
@@ -464,7 +464,7 @@ impl BaguaNet {
                 // let send_nbytes = data.len().to_be_bytes();
                 // ctrl_stream.write_all(&send_nbytes[..]).await.unwrap();
                 ctrl_stream.write_u32(data.len() as u32).await.unwrap();
-                println!(
+                tracing::debug!(
                     "send to {:?} target_nbytes={}",
                     ctrl_stream.peer_addr(),
                     data.len()
@@ -584,7 +584,7 @@ impl BaguaNet {
                     }
                 };
 
-                println!(
+                tracing::debug!(
                     "{:?} recv target_nbytes={}",
                     ctrl_stream.local_addr(),
                     target_nbytes
