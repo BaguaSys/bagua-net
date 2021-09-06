ffc50309a47d5d3a4e684726573b7a4af5e2ab18
shjwudp <shjwudp@gmail.com>
2021-09-06 21:47:06 +0800

    ..

--- src/bagua_net.rs
+++ src/bagua_net.rs
@@ -277,7 +277,7 @@ impl BaguaNet {
                 .parse()
                 .unwrap(),
             tokio_rt: tokio::runtime::Builder::new_multi_thread()
-                .worker_threads(4)
+                .worker_threads(8)
                 .enable_all()
                 .build()
                 .unwrap(),
